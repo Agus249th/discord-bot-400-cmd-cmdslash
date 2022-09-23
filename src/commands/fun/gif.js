83e358d1a0dadaae25cd5332f2d87adcbcbda4f9
[T]ruchorko[69] <61384359+luis498@users.noreply.github.com>
2022-09-23 18:41:46 -0500

    first commit

---
+++ src/commands/fun/gif.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const { config } = require('dotenv');
+
+module.exports = async (client, interaction, args) => {
+    const msg = interaction.options.getString('text');
+
+    if (!msg) return client.errUsage({ usage: "gif [text]", type: 'editreply' }, interaction);
+
+    var giphy = require('giphy-api')(process.env.GIPHY_TOKEN);
+
+    giphy.random(msg, function (err, res) {
+        client.embed({
+            title: `📺・${msg} Gif`,
+            image: `https://media1.giphy.com/media/${res.data.id}/giphy.gif`,
+            type: 'editreply'
+        }, interaction);
+    });
+}
+
+ 
